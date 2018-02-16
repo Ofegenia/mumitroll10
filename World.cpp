61d8d0449a1eda93723e7a74c46707be49b2ea2e
Maestrik <v_minyaev@inbox.ru>
2018-02-16 12:06:22 +0300

    e aborts the commit.

--- World.cpp
+++ World.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout<<"Hello Nikitoska!";
-	cout << "Hello Dan";
+	cout<<"Hello Nikitoska!"<<endl;
+	cout << "Hello Dan"<<endl;
+	cout << "Privet Volod'ka"<<endl;
 }
